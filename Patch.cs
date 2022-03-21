3e110547791539a517aef15c15307ffb0e9d9443
RZFury <48477044+rzfury@users.noreply.github.com>
2022-03-22 00:13:18 +0700

    Initial Commit

---
+++ Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace NoFecundityNerf.Patch
+{
+    public static class NoFecundityNerf
+    {
+        [HarmonyPatch(typeof(DiskCardGame.DrawCopy))]
+        [HarmonyPatch("CardToDrawTempMods", MethodType.Getter)]
+        [HarmonyPostfix]
+        static void Postfix(ref System.Collections.Generic.List<DiskCardGame.CardModificationInfo> __result)
+        {
+            if(SaveFile.IsAscension)
+            {
+                __result = null;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using BepInEx;
+
+namespace NoFecundityNerf
+{
+    [BepInPlugin("rzfury.noFecundityNerf", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($" {PluginInfo.PLUGIN_NAME} is loaded!");
+            new Harmony(PluginInfo.PLUGIN_GUID).PatchAll();
+        }
+    }
+}
